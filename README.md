e3afa8ef3bf28f2a706c3c42602aa1d3fd543cff
Alexander Lang <alex@upstre.am>
2015-12-23 15:00:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ember-deploy-cobot-scss
+
+This is the adapter implementation to use ember-cli-deploy to upload scss files to the Cobot SCSS service.
+
+# Configuration
+
+In your _deploy.js_ add the following:
---
+++ index.js
@@ -0,0 +1,45 @@
+var BasePlugin = require('ember-cli-deploy-plugin');
+var minimatch = require('minimatch');
+var Uploader = require('./lib/uploader');
+
+module.exports = {
+  name: 'ember-cli-deploy-cobot-scss',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      defaultConfig: {
+        srcDir: 'scss',
+        distDir: function(context) {
+          return context.distDir;
+        },
+        distFiles: function(context) {
+          return context.distFiles || [];
+        },
+      },
+      requiredConfig: ['accessToken', 'bundleUrl'],
+
+      upload: function(context) {
+        var that = this;
+        var distFiles = this.readConfig('distFiles');
+        var filesToUpload = distFiles.filter(minimatch.filter(this.readConfig('srcDir') + '/**/*.scss', { matchBase: true }));
+        this.log('Uploading SCSS...');
+        this.log('Files to upload: ' + filesToUpload, {verbose: true});
+        var uploader = new Uploader({
+          accessToken: this.readConfig('accessToken'),
+          bundleUrl: this.readConfig('bundleUrl'),
+          srcDir: this.readConfig('srcDir'),
+          baseDir: this.readConfig('distDir')
+        });
+        return uploader.uploadFiles(filesToUpload).then(function() {
+          that.log('Done uploading SCSS.');
+        }, function(error) {
+          that.log('Error uploading SCSS: ' + error, { color: 'red' });
+        });
+      }
+    });
+
+    return new DeployPlugin();
+  }
+};
